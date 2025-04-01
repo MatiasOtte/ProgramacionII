814b38838b51901495fbf7bdec0a4426bdad182c
Matías Otte <ottematias@gmail.com>
2025-04-01 15:45:32 -0300

    Hola

--- Proyectos/PreguntasDelCursoBD/src/main/java/com/cerp/Main.java
+++ Proyectos/PreguntasDelCursoBD/src/main/java/com/cerp/Main.java
@@ -32,8 +32,7 @@ public class Main {
     public static void main(String[] args) {
         
         // Crear instancia de FileHandler para cargar las preguntas desde preguntas.data
-        //FileHandler<Pregunta> fileHandler = new FileHandler<Pregunta>("D:\\_VSCode\\_Programacion3-JAVA\\Curso-JAVA\\Proyectos\\PreguntasDelCursoMVC\\preguntas.data");
-        FileHandler<Pregunta> fileHandler = new FileHandler<Pregunta>("C:\\Users\\55747485\\IdeaProjects\\ProgramacionII\\Proyectos\\PreguntasDelCursoMVC\\src\\main\\resources\\preguntas.data");
+        FileHandler<Pregunta> fileHandler = new FileHandler<Pregunta>("C:\\Users\\55747485\\IdeaProjects\\ProgramacionII\\Proyectos\\PreguntasDelCursoBD\\src\\main\\resources\\preguntas.data");
         List<Pregunta> modelo = new ArrayList<>();        
 
         try {

c6c40a8c2e94c635e6352ba1963dba2a67f8fb7d
Matías Otte <ottematias@gmail.com>
2025-04-01 16:41:16 -0300

    Hola

--- Proyectos/PreguntasDelCursoBD/src/main/java/com/cerp/Main.java
+++ Proyectos/PreguntasDelCursoBD/src/main/java/com/cerp/Main.java
@@ -32,7 +32,7 @@ public class Main {
     public static void main(String[] args) {
         
         // Crear instancia de FileHandler para cargar las preguntas desde preguntas.data
-        //FileHandler<Pregunta> fileHandler = new FileHandler<Pregunta>("C:\\Users\\55747485\\IdeaProjects\\ProgramacionII\\Proyectos\\PreguntasDelCursoBD\\src\\main\\resources\\preguntas.data");
+        FileHandler<Pregunta> fileHandler = new FileHandler<Pregunta>("C:\\Users\\55747485\\IdeaProjects\\ProgramacionII\\Proyectos\\PreguntasDelCursoBD\\src\\main\\resources\\preguntas.data");
 
         List<Pregunta> modelo = new ArrayList<>();
 
